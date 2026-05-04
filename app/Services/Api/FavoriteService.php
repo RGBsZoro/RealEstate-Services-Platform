9629eac60ab71d2a863caa458b1e65a8e2a6f3c7
wasem <wasemalhariri13@gmail.com>
2026-05-04 13:45:03 +0300

    add favorites

---
+++ app/Services/Api/FavoriteService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Models\Service;
+use App\Models\User;
+
+class FavoriteService
+{
+    public function getUserFavorites(User $user)
+    {
+        return $user->favoriteServices()
+            ->with([
+                'media',
+                'businessAccount:id,user_id,name'
+            ])
+            ->paginate(15);
+    }
+
+    public function addFavorite(User $user, array $data)
+    {
+        $user->favoriteServices()->attach($data['service_id']);
+    }
+
+    public function removeFavorite(User $user, Service $service)
+    {
+        $user->favoriteServices()->detach($service->id);
+    }
+}
